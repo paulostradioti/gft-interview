035cd643bd316f391e0fdf609daf3069d724c373
paulostradioti <paulo@paulo.eti.br>
2015-04-14 10:54:47 -0300

    BugFix

--- Domain.Test/OrderTest.cs
+++ Domain.Test/OrderTest.cs
@@ -11,14 +11,16 @@ namespace Domain.Test
         public TestContext TestContext { get; set; }
 
         [TestMethod]
-        [DeploymentItem("GetTimeOfDayValidInput.xls")]
-        [DataSource("System.Data.Odbc", "Driver={Microsoft Excel Driver (*.xls)};DBQ=GetTimeOfDayValidInput.xls;defaultdir=.", "Plan1$", DataAccessMethod.Sequential)]                
+        [DeploymentItem("GetTimeOfDay_ValidInput.xls")]
+        [DataSource("System.Data.Odbc", "Driver={Microsoft Excel Driver (*.xls)};DBQ=GetTimeOfDay_ValidInput.xls;defaultdir=.", "Plan1$", DataAccessMethod.Sequential)]                
         public void TestGetTimeOfDay()
         {
             var userInput = Convert.ToString(TestContext.DataRow["Input"]);
             var order = new Order(userInput);
 
             var expectedOutput = Convert.ToString(TestContext.DataRow["Output"]);
+            
+            order.GetTimeOfDay();
 
             Assert.AreEqual(order.TimeOfDay.ToString().ToLower(), expectedOutput);
         }
